c91b3b452071f4de84b37a5faadf071736b5ec60
Dmitriy <bonum.cattus@gmail.com>
2024-11-23 20:48:04 +0300

    try 40

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://quistaker.github.io',
+  site: 'https://quistaker.github.io/galactic-gamma',
   base: 'galactic-gamma',
 });
